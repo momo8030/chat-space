dbbcef55e09d154464d82deef948e6959a5b7c21
momo8030 <momo5938@icloud.com>
2019-11-24 17:37:21 +0900

    Automatic-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
 
   function buildHTML(message){
             var image = ( message.image ) ? `<img class="mesage__lower-info__image" src=${message.image}>` : '' ; 
-            var html = `<div class="message">
+            var html = `<div class="message", data-message-id="${message.id}">
                           <div class="message__upper-info">
                             <p class="message__upper-info__talker">
                             ${message.user_name}
@@ -38,10 +38,34 @@ $(function(){
       $('.messages').append(html);
       $('form')[0].reset();
       $('input').prop('disabled', false)
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},50);
     })
     .fail(function(){
       alert ('メッセージ送信に失敗しました');
     })
   })
-})
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/))　{
+      var last_message_id = $('.message:last').data('message-id');
+      $.ajax({
+        url: 'api/messages',
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML = buildHTML(message);
+          $(".messages").append(insertHTML);
+        })
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},50);
+      })
+      .fail(function() {
+        alert ('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
+});
