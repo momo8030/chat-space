11cbe95528d4322433424acef2c63a148c3c2d4c
momo8030 <momo5938@icloud.com>
2019-11-30 21:14:54 +0900

    Deploy

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -59,8 +59,8 @@ $(function(){
         messages.forEach(function(message) {
           insertHTML = buildHTML(message);
           $(".messages").append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},50);
         })
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},50);
       })
       .fail(function() {
         alert ('自動更新に失敗しました');
